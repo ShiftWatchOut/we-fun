62f4c189b11acd878360fe7fa0431f952659aaf0
shift <2080717464@qq.com>
2022-11-06 14:04:36 +0800

    单发 fetch

--- crawler/main.go
+++ crawler/main.go
@@ -1,52 +1,25 @@
 package main
 
 import (
-	"image"
-	"image/color"
-	"image/gif"
+	"fmt"
 	"io"
-	"math"
-	"math/rand"
+	"net/http"
 	"os"
-	"time"
-)
-
-var palette = []color.Color{color.White, color.Black}
-
-const (
-	whiteIndex = 0
-	blackIndex = 1
 )
 
 func main() {
-	rand.Seed(time.Now().UTC().UnixNano())
-	lissajous(os.Stdout)
-}
-
-func lissajous(out io.Writer) {
-	const (
-		cycles  = 5
-		res     = 0.001
-		size    = 100
-		nframes = 64
-		delay   = 8
-	)
-
-	freq := rand.Float64() * 3
-	anim := gif.GIF{LoopCount: nframes}
-	phase := 0.0
-	for i := 0; i < nframes; i++ {
-		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
-		img := image.NewPaletted(rect, palette)
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
-			x := math.Sin(t)
-			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
 		}
-		phase += 0.1
-		anim.Delay = append(anim.Delay, delay)
-		anim.Image = append(anim.Image, img)
+		b, err := io.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
 	}
-	gif.EncodeAll(out, &anim)
-	// build 之后 .\main.exe > out.gif
 }

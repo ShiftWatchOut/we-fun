b8c96aceca5715639507072f466a39cce9e5acb7
shift <2080717464@qq.com>
2023-01-03 23:38:29 +0800

    ✨ feat: 通过接口退出

--- crawler/main.go
+++ crawler/main.go
@@ -18,7 +18,29 @@ func main() {
 	router.GET("/sql-test", sqlTest)
 	router.GET("/ask-for-login", askForLogin)
 
-	router.Run()
+	done := make(chan bool)
+	router.GET("/secret-kill-server", func(ctx *gin.Context) {
+		if bot != nil {
+			self, err := bot.GetCurrentUser()
+			if err != nil {
+				ctx.String(200, "Kill error GetCurrentUser")
+				bot = nil
+			}
+			fh, err := self.FileHelper()
+			if err != nil {
+				ctx.String(200, "Kill error FileHelper")
+				bot = nil
+			}
+			self.SendTextToFriend(fh, "server killed by request")
+			bot.Logout()
+		}
+		done <- true
+	})
+
+	go func() {
+		router.Run()
+	}()
+	<-done
 }
 
 // 判断所给路径文件/文件夹是否存在

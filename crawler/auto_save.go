488e20c9bb9fe5d4dc1a6cb535dfd3e58bade727
shift <2080717464@qq.com>
2023-01-30 23:53:25 +0800

    🎈 perf: 自动展示 diff

---
+++ crawler/auto_save.go
@@ -0,0 +1,68 @@
+//go:build ignore
+// +build ignore
+
+package main
+
+import (
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/exec"
+	"sort"
+	"strings"
+)
+
+const url_1 = "http://localhost:8080/save-group-info"
+const url_2 = "http://localhost:8080/secret-kill-server"
+
+func main() {
+	fmt.Println("auto save running...")
+	resp, err := http.Get(url_1)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+		os.Exit(1)
+	}
+	b, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url_1, err)
+		os.Exit(1)
+	}
+	fmt.Printf("fetch: complete %s", b)
+	_, _ = http.Get(url_2)
+	// json 文件读取
+	pwd, _ := os.Getwd()
+	fileInfoList, _ := ioutil.ReadDir(pwd)
+	// 过滤
+	jsonFileList := make(MyFileInfo, 0)
+	for f := range fileInfoList {
+		file := fileInfoList[f]
+		filename := file.Name()
+		if strings.Contains(filename, ".json") && strings.Contains(filename, "-") {
+			jsonFileList = append(jsonFileList, file)
+		}
+	}
+	// 时间先后排序，取最后两个
+	sort.Sort(jsonFileList)
+	lenJson := len(jsonFileList)
+	file1, file2 := jsonFileList[lenJson-2], jsonFileList[lenJson-1]
+	fmt.Println("last two file are:", file1.Name(), file2.Name())
+	// 打开文件 diff
+	exec.Command("code", "-d", file1.Name(), file2.Name()).Run()
+}
+
+type MyFileInfo []fs.FileInfo
+
+func (fi MyFileInfo) Len() int {
+	return len(fi)
+}
+
+func (fi MyFileInfo) Swap(i, j int) {
+	fi[i], fi[j] = fi[j], fi[i]
+}
+
+func (fi MyFileInfo) Less(i, j int) bool {
+	return fi[i].ModTime().Before(fi[j].ModTime())
+}
